415684612232001ce244374bfa843be0ec28c720
Mindmech <alex8008@bk.ru>
2023-02-05 23:36:04 +0300

    hw4 task 2 done + alternative

--- hw4/main.py
+++ hw4/main.py
@@ -11,7 +11,6 @@ input m = 2 4 6 8 10 12 10 8 6 4 2
 input n = 3 6 9 12 15 18 
 
 output = 6 12
-
 '''
 
 def input_int_list (length, message, comment = ""):
@@ -44,24 +43,38 @@ print(set1)
 
 '''
 
+def gen_ascending_list (length):
+    list = []
+    for i in range (length):
+        list.append(i+1)
+    return list
 
+# вариант 1 - суммирует количество выбранного места и двух соседних
 
+def sum_row_part(number, input_list, length = 3):
+    sum = 0
+    selection_list = []
+    for i in range (length):
+        selection_list.append(number - i)
+    if number == len (input_list):
+        selection_list [0] = 0
+    for item in selection_list:
+        sum += input_list[item]
+    return sum
 
+shrub_list_input = int(input('Введите количество кустов на грядке:'))
+shrub_input = int(input('Введите номер искомого куста:'))
+shrubs = gen_ascending_list(shrub_list_input)
+print(shrubs)
+print(f'Количество собранных ягод при выборе {shrub_input} куста = {sum_row_part(shrub_input, shrubs)}')
 
-'''
-Вычислить число c заданной точностью d
-Пример:
-- при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
-'''
+# вариант 2 - максимальное значение трех соседних элементов
 
-'''
-Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
-Пример:
-- k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
-'''
+def sum_max_part(input_list, length = 3):
+    max_sum = 0
+    for i in range (0, len(input_list)):
+        if sum(input_list[i:(i+length)]) > max_sum:
+            max_sum = sum(input_list[i:(i+length)])
+    return max_sum
 
-'''
-Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
-'''
+print(f'Максимальное количество собранных ягод за 1 заход = {sum_max_part(shrubs)}')
